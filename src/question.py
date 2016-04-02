e5653d7184dbecdd24b865e0c648c524c070d6fb
Shirish Kadam <shirishkadam35@yahoo.com>
2016-04-02 23:22:33 +0530

    Playing with SpaCy

--- src/question.py
+++ src/question.py
@@ -2,35 +2,29 @@
 
 # ----------Imports------------
 
-import re
-import nltk
-#from nltk.tag.stanford import StanfordPOSTagger
+import spacy
 
 # -----------------------------
 
-question = raw_input("Q:")
-question = question.lower()                                             # Convert to lower case
+def print_coarse_pos(token):
+    print(token.pos_)
 
-symbols = re.findall(r'\W(?!\w)', question)                             # Separate symbols
-for sym in range(len(symbols)):
-    question = question.replace(symbols[sym], " "+symbols[sym])
-
-que_list = re.compile("\s+").split(question)                            # Split by space
-
-print(que_list)
-
-pos = nltk.pos_tag(que_list)
-
-print pos
-print nltk.chunk.conlltags2tree(question)
-print nltk.ne_chunk(question)
+def print_fine_pos(token):
+    print(token.tag_)
 
 
-#st = StanfordPOSTagger(r'/home/amit/stanford-postagger/models/english-bidirectional-distsim.tagger',r'/home/amit/stanford-postagger/stanford-postagger.jar')
-''' error comes here in st.tag instruction  '''
-
-#print st.tag('What is the airspeed of an unladen swallow ?'.split())
+# -----------------------------
 
 
-#forget about this ----> this is output--->[(u'What', u'WP'), (u'is', u'VBZ'), (u'the', u'DT'), (u'airspeed', u'NN'), (u'of', u'IN'), (u'an', u'DT'), (u'unladen', u'JJ'), (u'swallow', u'VB'), (u'?', u'.')]
+nlp = spacy.en.English()
 
+# question = raw_input("Q:")
+
+question = "Who is Linus Torvalds and where is he now?"
+
+doc = nlp(u""+question)
+
+for token in doc:
+    
+    print_fine_pos(token)
+    print_coarse_pos(token)
