dfbe1a049a491a1c867b6e4fee79713ff1f254ed
Shirish Kadam <shirishkadam35@gmail.com>
2016-09-19 00:30:09 +0530

    Wordnet Using TextBlob - Added Similarity Check

--- src/wordnet.py
+++ src/wordnet.py
@@ -1,5 +1,8 @@
 
 from textblob import Word
+from textblob.wordnet import Synset
+
+"""--------------- TextBlob --------------"""
 
 word = Word('plant')
 
@@ -18,3 +21,26 @@ for i in range(len(word.synsets)):
     # meronyms are components or substances that make up the item
     print(lemmas.member_holonyms())
     print(lemmas.part_meronyms())
+
+"""
+    Given that synsets can be organized as a graph, as shown above, we can measure the similarity of synsets based on
+    the shortest path between them.
+    This is called the path similarity, and it is equal to 1 / (shortest_path_distance(synset1, synset2) + 1).
+    It ranges from 0.0 (least similar) to 1.0 (identical).
+"""
+
+rootword = Synset("tiger.n.02")
+predator = Synset('lion.n.01')
+mammal = Synset('cat.n.01')
+reptile = Synset('snake.n.01')
+organ = Synset('heart.n.02')
+
+similar = {
+            rootword: rootword.path_similarity(rootword),
+            predator: rootword.path_similarity(predator),
+            mammal: rootword.path_similarity(mammal),
+            reptile: rootword.path_similarity(reptile),
+            organ: rootword.path_similarity(organ)
+        }
+
+print(rootword,">>",similar)
