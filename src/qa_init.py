b801b669c8380a2f7e8d8d734cab86382f7f1fac
Shirish Kadam <shirishkadam35@yahoo.com>
2017-02-19 00:09:19 +0530

    Added regular exp for symbols

--- src/qa_init.py
+++ src/qa_init.py
@@ -8,14 +8,15 @@ def spell_check(input_question):
     pattern = "\W"
     prog = re.compile(pattern)
 
-    input_question_word_list = input_question.split(" ")
+    input_question_word_list = input_question.split()
     en_dict = enchant.Dict("en_US")
     for word_index in range(len(input_question_word_list)):
-        if not en_dict.check(input_question_word_list[word_index]) and prog.match(input_question_word_list[word_index]) is not None:
+        if not en_dict.check(input_question_word_list[word_index]) and prog.match(input_question_word_list[word_index]) is None:
             correct_word = autocorrect.spell(input_question_word_list[word_index])
             input_question_word_list[word_index] = correct_word
-    return str(input_question_word_list)
+    return " ".join(input_question_word_list)
 
 
 input_question = "Why are ya stopin Googling bro, turn the arw up ?"
-print(spell_check(input_question))
+input_question_c = spell_check(input_question)
+print(input_question_c)
