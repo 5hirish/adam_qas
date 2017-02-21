95431045827c746dff7be3c96e6940329db1abd4
Shirish Kadam <shirishkadam35@yahoo.com>
2017-02-21 23:49:41 +0530

    Pandas data extraction, data frame and pre-processing for question classifier

--- src/qclassifier.py
+++ src/qclassifier.py
@@ -2,3 +2,25 @@ from sklearn import datasets
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn.svm import LinearSVC
 import pandas
+from sklearn.preprocessing import LabelBinarizer
+
+
+def get_data_info(dta):
+    print(dta.head())
+    print(dta.info())
+    print(dta.describe())
+    print(dta.columns)
+    print(dta.groupby('WH').describe())
+
+
+def pre_process(dta):
+    X_train = pandas.get_dummies(dta)
+    return X_train
+
+pandas.set_option('max_rows', 7)
+dta = pandas.read_csv('corpus/qclassifier_trainer.csv', sep='|')
+# get_data_info(dta)
+
+X_train = pre_process(dta)
+print(X_train.shape)
+
