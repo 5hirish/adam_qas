311bae05573e57e69b449b07eeeb5c428a534eb2
Shirish Kadam <shirishkadam35@yahoo.com>
2017-02-21 20:12:46 +0530

    Training CSV added

--- src/question_classifier.py
+++ src/question_classifier.py
@@ -9,14 +9,6 @@ Bi-gram = Part of Speech
 
 
 def process_question(question, qclass, en_nlp):
-    pass
-
-
-def read_input_file(fp, en_nlp):
-    # question = "How did serfdom develop in and then leave Russia ?"
-    for line in fp:
-        list_line = line.split("")
-        process_question(question, qclass, en_nlp)
     en_doc = en_nlp(u'' + question)
     for sent in en_doc.sents:
         wh_bi_gram = []
@@ -32,18 +24,41 @@ def read_input_file(fp, en_nlp):
             if token.dep_ == "ROOT":
                 root_token = token.tag_
 
-        print(wh_pos, wh_nbor_pos)
-        print(wh_bi_gram)
-        print(root_token)
-        with open('corpus/qclassifier_trainer.csv', 'w', newline='') as csv_fp:
+        # print(wh_pos, wh_nbor_pos)
+        # print(wh_bi_gram)
+        # print(root_token)
+        with open('corpus/qclassifier_trainer.csv', 'a', newline='') as csv_fp:
             csv_fp_writer = csv.writer(csv_fp, delimiter='|')
-            csv_fp_writer.writerow(['Question', 'WH-Bigram', 'WH-POS', 'WH-NBOR-POS', 'Root-POS', 'Class'])
-            csv_fp_writer.writerow([question, " ".join(wh_bi_gram), wh_pos, wh_nbor_pos, root_token])
+            csv_fp_writer.writerow([question, " ".join(wh_bi_gram), wh_pos, wh_nbor_pos, root_token, qclass])
             csv_fp.close()
 
+
+def read_input_file(fp, en_nlp):
+    # question = "How did serfdom develop in and then leave Russia ?"
+    for line in fp:
+        list_line = line.split(" ")
+        qclass_list = list_line[0].split(":")
+        question = " ".join(list_line[1:len(list_line)])
+        question = question.strip("\n")
+        qclass = qclass_list[0]
+        # print(qclass, question)
+        process_question(question, qclass, en_nlp)
+
+
+def clean_old_data():
+    with open('corpus/qclassifier_trainer.csv', 'w', newline='') as csv_fp:
+        csv_fp_writer = csv.writer(csv_fp, delimiter='|')
+        csv_fp_writer.writerow(['Question', 'WH-Bigram', 'WH-POS', 'WH-NBOR-POS', 'Root-POS', 'Class'])
+        csv_fp.close()
+
+
+clean_old_data()
 en_nlp = spacy.load("en")
+
 with open('corpus/qclassification_data.txt', 'r') as fp:
     read_input_file(fp, en_nlp)
+    fp.close()
+    print("CSV Data Trained...")
 
 
 
