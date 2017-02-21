6c14afed78650b3d0f60131f5f72d33bb0a23502
Shirish Kadam <shirishkadam35@yahoo.com>
2017-02-22 00:18:05 +0530

    CART Decision Tree example for QClass

--- src/qclassifier.py
+++ src/qclassifier.py
@@ -2,6 +2,7 @@ from sklearn import datasets
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn.svm import LinearSVC
 import pandas
+from sklearn.tree import DecisionTreeClassifier, export_graphviz
 from sklearn.preprocessing import LabelBinarizer
 
 
@@ -14,13 +15,17 @@ def get_data_info(dta):
 
 
 def pre_process(dta):
-    X_train = pandas.get_dummies(dta)
-    return X_train
+    return pandas.get_dummies(dta)
 
 pandas.set_option('max_rows', 7)
 dta = pandas.read_csv('corpus/qclassifier_trainer.csv', sep='|')
 # get_data_info(dta)
 
+y = dta.pop('Class')
+print(dta.info())
 X_train = pre_process(dta)
 print(X_train.shape)
 
+dtree = DecisionTreeClassifier(random_state=0, max_depth=2)
+dtree.fit(X_train, y)
+export_graphviz(dtree, feature_names=X_train.columns)
