b8dd96c04b0a357819ca0d1815d95e8dff6b49fc
Shirish Kadam <shirishkadam35@yahoo.com>
2017-02-22 22:45:05 +0530

    SVM implemented for Question Classifier

--- src/qclassifier.py
+++ src/qclassifier.py
@@ -11,11 +11,42 @@ def get_data_info(dta):
     print(dta.info())
     print(dta.describe())
     print(dta.columns)
-    print(dta.groupby('WH').describe())
 
 
 def pre_process(dta):
     return pandas.get_dummies(dta)
+
+
+def transform_data_matrix(X_train, X_predict):
+    X_train_columns = list(X_train.columns)
+    X_predict_columns = list(X_predict.columns)
+
+    X_trans_columns = list(set(X_train_columns + X_predict_columns))
+    # print(X_trans_columns, len(X_trans_columns))
+
+    trans_data_train = {}
+
+    for col in X_trans_columns:
+        if col not in X_train:
+            trans_data_train[col] = [0 * len(X_train.index)]
+        else:
+            trans_data_train[col] = list(X_train[col])
+
+    XT_train = pandas.DataFrame(trans_data_train)
+    # get_data_info(XT_train)
+
+    trans_data_predict = {}
+
+    for col in X_trans_columns:
+        if col not in X_predict:
+            trans_data_predict[col] = 0
+        else:
+            trans_data_predict[col] = list(X_predict[col])  # KeyError
+
+    XT_predict = pandas.DataFrame(trans_data_predict)
+    # get_data_info(XT_predict)
+
+    return XT_train, XT_predict
 
 
 def naive_bayes_classifier(X_train, y, X_predict):
@@ -32,7 +63,7 @@ def support_vector_machine(X_train, y, X_predict):
     return prediction
 
 
-def get_question_predict_data(question, en_nlp, column_list):
+def get_question_predict_data(question, en_nlp):
     en_doc = en_nlp(u'' + question)
     sent_list = list(en_doc.sents)
     sent = sent_list[0]
@@ -58,7 +89,6 @@ def get_question_predict_data(question, en_nlp, column_list):
 
 
 en_nlp = spacy.load("en")
-pandas.set_option('max_rows', 7)
 dta = pandas.read_csv('corpus/qclassifier_trainer.csv', sep='|')
 # get_data_info(dta)
 
@@ -69,13 +99,19 @@ dta.pop('WH-Bigram')
 X_train = pre_process(dta)
 
 # print(X_train.shape)
-column_list = list(X_train.columns)
+
 # print(len(column_list))
-question = 'Who is Linus Torvalds ?'
-question_data = get_question_predict_data(question, en_nlp, column_list)
+
+# question = 'Who is Linus Torvalds ?'
+question = 'What is the colour of apple ?'
+
+
+question_data = get_question_predict_data(question, en_nlp)
 X_predict = pre_process(question_data)
-print(X_predict)
-print(X_train)
+# print(X_predict)
+# print(X_train)
+
+X_train, X_predict = transform_data_matrix(X_train, X_predict)
 
 # print(naive_bayes_classifier(X_train, y, X_predict))
 print(support_vector_machine(X_train, y, X_predict))
