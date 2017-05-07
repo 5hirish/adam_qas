64a7593feb12ff74c51de2c0a4109f0b48755d67
Shirish Kadam <shirishkadam35@yahoo.com>
2017-05-07 06:56:25 +0530

    Added Feature Trainer

--- src/data/feature_extractor.py
+++ src/data/feature_extractor.py
@@ -1,21 +1,3 @@
-'''
-
-Feature Extractor
-
-Based on Question Type
-Feature Extraction
-
-Candidate selection: Here, we extract all possible words, phrases, terms or concepts (depending on the task) that can potentially be keywords.
-Properties calculation: For each candidate, we need to calculate properties that indicate that it may be a keyword.
-Scoring and selecting keywords: All candidates can be scored by either combining the properties into a formula, or using a machine learning technique to determine probability of a candidate being a keyword
-
-# QType
-# Compound Phrases
-# Dependency Labels
-# POS Tags
-# Collapsed Phrases
-'''
-
 import spacy
 import csv
 
@@ -25,7 +7,8 @@ def get_detail(sent):
         print(token.text, token.lemma_, token.tag_, token.ent_type_, token.dep_, token.head)
 
 
-def get_compound_nouns(token, token_text, en_doc):
+def get_compound_nouns(token, token_text):
+
     ptoken = token
 
     print(token.text, token.dep_)
@@ -40,6 +23,7 @@ def get_compound_nouns(token, token_text, en_doc):
         token_text = token_text + " " + en_doc[token.i + 1].text
         token = en_doc[token.i + 1]
         print("R", token_text)
+
     return token_text
 
 
@@ -64,7 +48,7 @@ def get_noun_chunk(sent, en_doc, keywords):
     for token in sent:
         if token.tag_ == "NN" or token.tag_ == "NNP" or token.tag_ == "NNPS" or token.tag_ == "NNS":
             if token.dep_ != "compound":
-                token_text = get_compound_nouns(token, token.text, en_doc)
+                token_text = get_compound_nouns(token, token.text)
                 token_text = get_adj_phrase(token, token_text)
                 keywords.append(token_text)
 
@@ -84,51 +68,17 @@ def get_noun_chunk(sent, en_doc, keywords):
 
     return root, keywords
 
+question = "What's the American dollar equivalent for 8 pounds in the U.K. ?"
 
-def write_csv(question, qclass, keywords):
-    with open('corpus/feature_trainer.csv', 'a', newline='') as csv_fp:
-        csv_fp_writer = csv.writer(csv_fp, delimiter='|')
-        csv_fp_writer.writerow([question, qclass, keywords])
-        csv_fp.close()
-
-
-def process_question(question, qclass, en_nlp):
-
-    en_doc = en_nlp(u'' + question)
-    keywords = []
-
-    for sent in en_doc.sents:
-        # get_detail(sent)
-        root, keywords = get_noun_chunk(sent, en_doc, keywords)
-        keywords.append(root)
-
-    write_csv(question, qclass, keywords)
-    # print(keywords)
-    del keywords[:]
-
-
-def read_input_file(fp, en_nlp):
-    # question = "How did serfdom develop in and then leave Russia ?"
-    for line in fp:
-        list_line = line.split(" ")
-        qclass_list = list_line[0].split(":")
-        question = " ".join(list_line[1:len(list_line)])
-        question = question.strip("\n")
-        qclass = qclass_list[0]
-        # print(qclass, question)
-        process_question(question, qclass, en_nlp)
-
-
-def clean_old_data():
-    with open('corpus/feature_trainer.csv', 'w', newline='') as csv_fp:
-        csv_fp_writer = csv.writer(csv_fp, delimiter='|')
-        csv_fp_writer.writerow(['Question', 'Class', 'Keywords'])
-        csv_fp.close()
-
-clean_old_data()
 en_nlp = spacy.load('en_core_web_md')
 
-with open('corpus/qclassification_data.txt', 'r') as fp:
-    read_input_file(fp, en_nlp)
-    fp.close()
-    print("CSV Data Trained...")
+en_doc = en_nlp(u'' + question)
+keywords = []
+
+for sent in en_doc.sents:
+    get_detail(sent)
+    root, keywords = get_noun_chunk(sent, en_doc, keywords)
+    keywords.append(root)
+
+print(question)
+print(keywords)
---
+++ src/query_const.py
@@ -0,0 +1,12 @@
+'''
+
+The Query is constructed just to show relationship amongst different features in the feature set.
+A conjunction is a part of speech that is used to connect words, phrases, clauses, or sentences. 
+
+'''
+
+import spacy
+import csv
+
+
+en_nlp = spacy.load('en_core_web_md')
