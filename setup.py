5489acd3dc827fb70226dbea799676858decd331
Shirish Kadam <shirishkadam35@yahoo.com>
2018-01-12 02:48:15 +0530

    Fixed setup.py and travis configuration

--- setup.py
+++ setup.py
@@ -2,21 +2,59 @@
 # -*- coding: utf-8 -*-
 """
     Setup file for qas.
-
-    This file was generated with PyScaffold 2.5.8, a tool that easily
-    puts up a scaffold for your new Python project. Learn more under:
-    http://pyscaffold.readthedocs.org/
 """
 
 import sys
-from setuptools import setup
+import os
+import io
+import contextlib
+from setuptools import setup, find_packages
+
+
+@contextlib.contextmanager
+def chdir(new_dir):
+    old_dir = os.getcwd()
+    try:
+        os.chdir(new_dir)
+        sys.path.insert(0, new_dir)
+        yield
+    finally:
+        del sys.path[0]
+        os.chdir(old_dir)
 
 
 def setup_package():
-    needs_sphinx = {'build_sphinx', 'upload_docs'}.intersection(sys.argv)
-    sphinx = ['sphinx'] if needs_sphinx else []
-    setup(setup_requires=['six', 'pyscaffold>=2.5a0,<2.6a0'] + sphinx,
-          use_pyscaffold=True)
+
+    base_dir = os.path.abspath(os.path.dirname(__file__))
+
+    with chdir(base_dir):
+        with io.open(os.path.join(base_dir, 'qas', 'about.py'), encoding='utf8') as fp:
+            about = {}
+            exec(fp.read(), about)
+
+    with io.open(os.path.join(base_dir, 'README.rst'), encoding='utf8') as f:
+        readme = f.read()
+
+    setup(name=about['__title__'],
+          packages=find_packages(),
+          description=about['__summary__'],
+          long_description=readme,
+          version=about['__version__'],
+          author=about['__author__'],
+          author_email=about['__email__'],
+          url=about['__uri__'],
+          license=about['__license__'],
+          install_requires=[
+              "autocorrect>=0.3.0",
+              "gensim>=3.0.1",
+              "lxml>=4.1.0",
+              "pandas>=0.21",
+              "pyenchant>=2.0.0",
+              "requests>=2.18.0",
+              "spaCy>=2.0.3",
+              "scikit-learn>=v0.19.1",
+              "wikipedia>=1.4.0"]
+          )
 
 
 if __name__ == "__main__":
