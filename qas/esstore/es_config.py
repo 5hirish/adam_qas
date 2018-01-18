848ef4eda6399a26e95638bd1807f6c99c8ee3f1
Shirish Kadam <shirishkadam35@yahoo.com>
2018-01-18 23:46:17 +0530

    Added config for Elasticsearch index

---
+++ qas/esstore/es_config.py
@@ -0,0 +1,8 @@
+
+__index_name__ = "adam_qas"
+__num_shards__ = 1
+__num_replicas__ = 0
+__doc_type__ = "article"
+
+__analyzer_std__ = "standard"
+__analyzer_en__ = "english"
--- qas/esstore/es_insert.py
+++ qas/esstore/es_insert.py
@@ -5,3 +5,4 @@ from qas.esstore.es_connect import ElasticSearchConn
 class ElasticSearchInsert:
     es = ElasticSearchConn()
     es_conn = es.get_db_connection()
+
