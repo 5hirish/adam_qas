6fefc216d45d50f90670a02ec5e5ca09911f9b81
Shirish Kadam <shirishkadam35@yahoo.com>
2018-03-11 19:03:05 +0530

    Added QA test sqlite data base.

---
+++ docs/resources.rst
@@ -0,0 +1,11 @@
+https://github.com/dapurv5/awesome-question-answering
+
+Datasets:
+
+https://research.fb.com/downloads/babi/ : The SimpleQuestions dataset
+
+https://nlp.stanford.edu/software/sempre/ : SEMPRE: Semantic Parsing with Execution
+
+https://rajpurkar.github.io/SQuAD-explorer/ : The Stanford Question Answering Dataset
+
+https://github.com/shuzi/insuranceQA : A question answering corpus in insurance domain
--- qas/sqlitestore/sqlt_connect.py
+++ qas/sqlitestore/sqlt_connect.py
@@ -13,7 +13,7 @@ Created by felix on 8/3/18 at 1:40 AM
 logger = logging.getLogger(__name__)
 
 
-class SqLiteMeta:
+class SqLiteMeta(type):
     _instances = {}
 
     def __call__(cls, *args, **kwargs):
@@ -26,18 +26,62 @@ class SqLiteManager(metaclass=SqLiteMeta):
 
     __test_db__ = CORPUS_DIR + "/" + QA_TEST_DATA
     __sqlt_conn__ = None
+    sqlt_cursor = None
+    table_name = "wikiqa"
 
     def __init__(self, db_name=__test_db__):
         self.__sqlt_conn__ = sqlite3.connect(db_name)
+        self.create_table()
 
     def get_db_cursor(self):
-        return self.__sqlt_conn__.cursor()
+        if self.sqlt_cursor is None:
+            self.sqlt_cursor = self.__sqlt_conn__.cursor()
+        return self.sqlt_cursor
 
     def commit_db(self):
         self.__sqlt_conn__.commit()
 
     def close_db_(self):
         self.__sqlt_conn__.close()
+
+    def create_table(self):
+        create_query = """CREATE TABLE IF NOT EXISTS """+self.table_name+""" (
+            Qid integer PRIMARY KEY,
+            Question text,
+            Class text,
+            Features text,
+            Query text,
+            Pages text,
+            Ranks text,
+            Answer text)
+        """
+        self.get_db_cursor().execute(create_query)
+        self.commit_db()
+
+    def insert_many_question(self, question_set):
+        insert_many_query = """INSERT INTO """+self.table_name+""" 
+            (Question) VALUES (?)"""
+        self.get_db_cursor().executemany(insert_many_query, question_set)
+        self.commit_db()
+
+    def get_question_count(self):
+        count_questions_query = """SELECT COUNT(*) FROM """+self.table_name
+        self.get_db_cursor().execute(count_questions_query)
+        result = self.get_db_cursor().fetchone()
+        return result[0]
+
+    def remove_all_data(self):
+        delete_all = """DELETE FROM """+self.table_name
+        self.get_db_cursor().execute(delete_all)
+        self.commit_db()
+
+    def remove_old_results(self):
+        update_many_query = """UPDATE """+self.table_name+""" 
+            SET Class = ?, Features = ?, Query = ?, Pages = ?, Ranks = ?, Answer = ?
+            WHERE (Class IS NOT NULL OR Features IS NOT NULL OR Query IS NOT NULL OR Pages IS NOT NULL
+                    OR Ranks IS NOT NULL OR Answer IS NOT NULL)"""
+        self.get_db_cursor().execute(update_many_query, (None, None, None, None, None, None))
+        self.commit_db()
 
 
 if __name__ == "__main__":
