ec6b99c0499f30ca37d22a08ae5e6cea447c5190
Shirish Kadam <shirishkadam35@yahoo.com>
2018-05-18 23:32:48 +0530

    Added script to print system info, elasticsearch mapping

---
+++ qas/sys_info.py
@@ -0,0 +1,17 @@
+import platform
+import elasticsearch
+
+from qas.esstore.es_connect import ElasticSearchConn
+from qas.esstore.es_config import __index_version__
+
+
+def get_system_info():
+    print("System:", platform.system())
+    print("Platform:", platform.version())
+    print("Python:", platform.python_version())
+    print("Elasticsearch:", str(elasticsearch.__version__))
+    print("Elasticsearch Mapping: ", ElasticSearchConn().get_index_mapping())
+
+
+if __name__ == "__main__":
+    get_system_info()
